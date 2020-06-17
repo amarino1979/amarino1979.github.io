2a171cc0cd739cd935c915c72968a34cce4312f0
Aaron Marino <aaron_marino@yahoo.com>
2020-06-17 18:46:20 -0400

    picked the api and added the key.  Added an array to push the data set into.

--- Suburb_Growing/app.js
+++ Suburb_Growing/app.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const baseURL = `http://harvesthelper.herokuapp.com/api/v1/plants?api_key=3d61aab31f7111f646d7d1985662a0ec`
+const api_key = `3d61aab31f7111f646d7d1985662a0ec`
+const queryType = `=`
+let titleQuery = ''
+let queryURL = baseURL
+
+console.log(baseURL)
+
+let gardenData = [];
+$(() => { 
+
+    $.ajax({
+        type: 'get', 
+        url: baseURL
+    }).then((dataSet) => {
+        gardenData = dataSet
+        console.log(gardenData)
+    }) 
+})
