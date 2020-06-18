52d3e611b877b7adf080591cdfe9a2af40d29422
Aaron Marino <aaron_marino@yahoo.com>
2020-06-18 17:34:13 -0400

    able to alphabetize the dropdown
    :wq

--- Suburb_Growing/app.js
+++ Suburb_Growing/app.js
@@ -7,22 +7,49 @@ let queryURL = baseURL
 console.log(baseURL)
 
 let gardenData = [];
+//gardenData
+// function myFunction() {
+//     gardenData.sort();
+// }
 $(() => { 
 
     $.ajax({
         type: 'get', 
         url: baseURL
     }).then((dataSet) => {
-        gardenData = dataSet
+        //populate a drop down by name by alphabetical order
+        gardenData = dataSet.sort((a, b) => (a.name > b.name) ? 1 : -1)
+        // found this line on https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
         console.log(gardenData)
         gardenData.forEach((element) => {
             let opElem = $(`<option> ${element.name} </option>`)
             $('#dropdown').append(opElem)
-        })
-    }) 
-    //populate a drop down by name
-    //
-    //need to add event listener for
+        $('#dropdown').on('click', (event) => {
+            event.preventDefault()
+            
 
+        })    
+        })
+    })  
+    // once populated create event listener for what is clicked on the drop down.
+    //also need to add a submit listener if they use search bar
+    //add a div container to push all the data pulled from api
     //
 })
+/* 
+list of classes needed to populate
+<h3> ${gardenData.name} </h3>
+            image_url?
+<h4> ${gardenData.description} </h4>
+<h4> ${gardenData.optimal_soil} </h4>
+<h4> ${gardenData.optimal_sun} </h4>
+<h4> ${gardenData.when_to_plant} </h4>
+<h4> ${gardenData.planting_considerations} </h4>
+<h4> ${gardenData.transplanting} </h4>
+<h4> ${gardenData.growing_from_seed} </h4>
+<h4> ${gardenData.spacing} </h4>
+<h4> ${gardenData.watering} </h4>
+<h4> ${gardenData.other_care} </h4>
+<h4> ${gardenData.pests} </h4>
+<h4> ${gardenData.harvesting} </h4>
+<h4> ${gardenData.storage_use} </h4> */
