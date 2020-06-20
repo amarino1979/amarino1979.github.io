76312ae47f2ade887b1f2dccfd69ecaf9d5965c0
Aaron Marino <aaron_marino@yahoo.com>
2020-06-20 15:37:17 -0400

    created 2nd ajax call and added an option tag in html for the dropdown menu

--- Suburb_Growing/app.js
+++ Suburb_Growing/app.js
@@ -23,18 +23,71 @@ $(() => {
                 //event.preventDefault()
                 console.log("hi");
                 const dropdownItem = $('#dropdown').val();
-                gardenData.push(dropdownItem)
+                gardenData.push(element)
                 //$(event.currentTarget).trigger('reset');
                 //console.log(dropdownItem);
                 // once alphabetized create event listener for what is clicked on the drop down.
             })
+            
         })
-        //console.log('#dropdown')
-    })
-    //also need to add a submit listener if they use search bar
-    //add a div container to push all the data pulled from api
-    //
+        document.addEventListener('input', function (event) {
+            // Only run on our select menu
+            if (event.target.id !== 'dropdown') return;
+            // The selected value
+            console.log(gardenData)
+            console.log(event.target.value);
+            const selectedVeg = gardenData.find(veg =>  veg.name === event.target.value )
+            console.log(selectedVeg.id)
+            // The selected option element
+            // *********************ajax call inside here *********************
+            // const getSelectedPlantData = () => {
+                $.ajax({
+                    type: 'get',
+                    url: `http://harvesthelper.herokuapp.com/api/v1/plants/${selectedVeg.id}?api_key=3d61aab31f7111f646d7d1985662a0ec`
+                }).then((data) => {
+                    console.log(data)                  
+                    $('#container').append(`
+                    <h3> ${data.name} </h3>
+                    <h4> Description: ${data.description} </h4>
+                    <h4> Optimal Soil: ${data.optimal_soil} </h4>
+                    <h4> Optimal Sun: ${data.optimal_sun} </h4>
+                    <h4> When to plant: ${data.when_to_plant} </h4>
+                    <h4> Planting Considerations: ${data.planting_considerations} </h4>
+                    <h4> Transplanting: ${data.transplanting} </h4>
+                    <h4> Growing from seeds: ${data.growing_from_seed} </h4>
+                    <h4> Spacing: ${data.spacing} </h4>
+                    <h4> Watering: ${data.watering} </h4>
+                    <h4> Other care: ${data.other_care} </h4>
+                    <h4> Pests: ${data.pests} </h4>
+                    <h4> Harvesting: ${data.harvesting} </h4>
+                    <h4> Storage Use: ${data.storage_use} </h4>`) 
+            }, (error) => {
+                console.log(error)
+            })
+    //    }
+            console.log(event.target.options[event.target.selectedIndex]);
+    }, false);
+    //console.log('#dropdown')
 })
+//also need to add a submit listener if they use search bar
+//add a div container to push all the data pulled from api
+//
+// Using the code from the morning exercise from w03d05:
+const $openBtn = $('#openModal');
+const $modal = $('#modal');
+const $submitBtn = $('#submit');
+
+const openModal = () => {
+    $modal.css('display', 'block');
+}
+const closeModal = () => {
+    $modal.css('display', 'none');
+}
+//looking into local storage
+$openBtn.on('click', openModal);
+$submitBtn.on('click', closeModal);
+    //clear out fields
+});
 /*
 list of classes needed to populate
 <h3> ${gardenData.name} </h3>
